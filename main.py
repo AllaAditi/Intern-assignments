02620bb08a62c4f32e9f10e0c7483a7ac705c459
Alla Aditi <allaaditi02@gmail.com>
2023-05-17 13:34:37 +0530

    committed

--- main.py
+++ main.py
@@ -7,5 +7,5 @@ app = FastAPI()
 app.include_router(item_router)
 if __name__ == '__main__':
     load_dotenv()
-    uvicorn.run(app="main:app", reload=True)
+    uvicorn.run(app="main:app", port=2345, reload=True)
 
